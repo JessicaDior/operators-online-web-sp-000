965cde9b4ae7f73854bec7ef24a6600b9fe72b48
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-23 18:40:50 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
-	if speed.between? (40) (60)
+	if speed.between? 40-60
 		return false
 	else return true
 	end
 end
 
 def not_safe?(speed)
-	speed >= 60 ? true : false
+	speed >= 40-60 ? true : false
 	end
